078caf1575f48b93053ffa83e4edb90c243f39ee
lytuan99 <lytuan270899@gmail.com>
2020-11-19 20:33:54 +0700

    update on ubuntu

---
+++ src/pages/userProfile/UserProfile/index.js
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+
+UserProfile = (props) => {
+    // add tổng thể trang profile trong này
+
+    //các component con sẽ dk nhét trong này
+
+}
+
+export default UserProfile;

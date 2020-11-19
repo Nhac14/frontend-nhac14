eae55b3f3d52f18530d88a17d0c69b7bfb6e7038
haild173094 <56255783+haild173094@users.noreply.github.com>
2020-11-19 12:27:00 +0700

    Login-RegisterPage

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import 'antd/dist/antd.css';
 import HeaderX from './pages/layout/HeaderX/HeaderX.js';
 import Content from './pages/Home/Content.js';
 import Footer from './pages/layout/Footer/Footer.js';
-
+import Login from './pages/layout/Login/Login.js';
+import Register from './pages/layout/Register/Register.js';
 
 function App(){
    
       return (
          <div>
             <HeaderX/>
-           
-            <Content /> 
+            <Register/>
+            {/* <Login/> */}
+            {/* <Content />  */}
             <Footer/>
          </div>
       );
---
+++ src/pages/layout/Login/Login.js
@@ -0,0 +1,46 @@
+import Form from 'antd/lib/form/Form';
+import React from 'react';
+import './Style_login.css';
+function Login() {
+    return (
+        <div className="container">
+            <Form>
+                <div className="box-auth-inside">
+                    <a href="#" className="a-img-logo"><img src="/icons/icon_white.png" alt="auth-img"></img></a>
+
+                    <div className="input-group">
+                        <label for="">Nhập tên đăng nhập hoặc email</label>
+                        <input type="text" name="account_name"></input>
+                        <p id="eg" data-default-text="VD: abc123, abcdef@gmail.com">VD: abc123, abcdef@gmail.com</p>
+                        <label>Nhập mật khẩu</label>
+                        <input type="password" name="account_Password"></input>
+                        {/* <p id="eg" data-default-text="VD: 0912345789, abc123, abcdef@gmail.com">VD: 0912345789, abc123, abcdef@gmail.com</p> */}
+                    </div>
+
+                    <button className="login-btn">Đăng nhập</button>
+                </div>
+                <div className="box-auth-panel">
+                    <h4 className="top">hoặc đăng nhập với </h4>
+               
+                     <ul className="a-login-ul">
+                        <li>
+                            <a href="#" className="a-login-fb">
+                                <img src="/icons/fb_icon.png" alt="auth-img"></img> Facebook
+                               
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="a-login-google">
+                                <img src="/icons/gg_icon.png" alt="auth-img"></img>    Google
+                            
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </Form>
+        </div>
+    )
+}
+
+export default Login
+

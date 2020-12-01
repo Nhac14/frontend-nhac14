d31a30de13eb05b52d5f20526e76cd6182e034e6
haild173094 <56255783+haild173094@users.noreply.github.com>
2020-12-01 22:28:47 +0700

    test2

--- src/pages/Admin/Singer/ListSinger.jsx
+++ src/pages/Admin/Singer/ListSinger.jsx
@@ -7,57 +7,90 @@ const ListSinger = () => {
     let data = [
         {
             key: '1',
-            firstName: 'John',
-            lastName: 'Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
+            name: 'John',
+            gender: 'Male',
+            age: 41,
+            avatar: '',
+            description:'',
+            favorites:'pop',
+            createdDate:'9/10/2001',
+            modifiedDate:'',
+            createdBy:'',
+            modifiedBy:'',
+            // tags: ['nice', 'developer'],
         },
         {
             key: '2',
-            firstName: 'Jim',
-            lastName: 'Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
+            name: 'Mixi',
+            gender: 'Male',
+            age: 30,
+            avatar: '',
+            description:'',
+            favorites:'pop',
+            createdDate:'20/11/2000',
+            modifiedDate:'',
+            createdBy:'',
+            modifiedBy:'',
+            // tags: ['loser'],
         },
         {
             key: '3',
-            firstName: 'Joe',
-            lastName: 'Black',
+            name: 'Pew',
+            gender: 'Male',
+            age: 31,
+            avatar: '',
+            description:'',
+            favorites:'country',
+            createdDate:'11/11/1999',
+            modifiedDate:'',
+            createdBy:'',
+            modifiedBy:'',
+            // tags: ['cool', 'teacher'],
+        },
+        {
+            key: '4',
+            name: 'Faker',
+            gender: 'Male',
+            age: 23,
+            avatar: '',
+            description:'',
+            favorites:'k-pop',
+            createdDate:'10/8/2010',
+            modifiedDate:'',
+            createdBy:'',
+            modifiedBy:'',
+            // tags: ['cool', 'teacher'],
+        },
+        {
+            key: '5',
+            name: 'TestName',
+            gender: 'Female',
             age: 32,
-            address: 'Sidney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
+            avatar: '',
+            description:'',
+            favorites:'pop',
+            createdDate:'20/11/2020',
+            modifiedDate:'',
+            createdBy:'',
+            modifiedBy:'',
+            // tags: ['cool', 'teacher'],
         },
         ];
     return ( <div>
             <Table dataSource={data}>
-                <ColumnGroup title="Name">
-                <Column title="First Name" dataIndex="firstName" key="firstName" />
-                <Column title="Last Name" dataIndex="lastName" key="lastName" />
-                </ColumnGroup>
+                
+                <Column title="Name" dataIndex="name" key="name" />
+                <Column title="Gender" dataIndex="gender" key="gender" />
+                
                 <Column title="Age" dataIndex="age" key="age" />
-                <Column title="Address" dataIndex="address" key="address" />
-                <Column
-                title="Tags"
-                dataIndex="tags"
-                key="tags"
-                render={tags => (
-                    <>
-                    {tags.map(tag => (
-                        <Tag color="blue" key={tag}>
-                        {tag}
-                        </Tag>
-                    ))}
-                    </>
-                )}
-                />
+                <Column title="Favorites" dataIndex="favorites" key="favorites" />
+                <Column title="CreatedDate" dataIndex="createdDate" key="createdDate" />
                 <Column
                 title="Action"
                 key="action"
                 render={(text, record) => (
                     <Space size="middle">
-                    <a>Invite {record.lastName}</a>
+                    <a>Invite {record.name}</a>
                     <a>Delete</a>
                     </Space>
                 )}

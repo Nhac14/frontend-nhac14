e32c5b5959a349604db49d3fbd1eb6e22581159a
haild173094 <56255783+haild173094@users.noreply.github.com>
2020-12-11 20:56:04 +0700

    new-singer

--- src/pages/Admin/Singer/NewSinger.jsx
+++ src/pages/Admin/Singer/NewSinger.jsx
@@ -1,11 +1,87 @@
-import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox, Select } from 'antd';
+// import { Select } from 'antd';
+import SelectGender from '../../Helper/SelectGender';
+import Avatar from '../../Helper/Upload-image-preview';
 
 
-const NewSinger = () => {
-    return ( <div>
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 10, span: 14 },
+};
 
-        <h1>this is NewSinger component</h1>
-    </div> );
+const { TextArea } = Input;
+
+const Demo = () => {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+const { Option } = Select;
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
 }
- 
-export default NewSinger;
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: 'Please input singer\'s name!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Gender"
+        name="gender"
+        rules={[{ required: true , message:'Plese select your gender'}]}
+      >
+         <SelectGender/>
+      </Form.Item>
+
+      <Form.Item
+        label="Age"
+        name="age"
+        rules={[{ required:true , message:'Plese enter your age'}]}
+      >
+         <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Favorites"
+        name="favorites"
+        rules={[{ required: false}]}
+      >
+        <TextArea rows={6}/>
+      </Form.Item>
+
+      <Form.Item
+        label="Avatar"
+        name="avatar"
+        rules={[{ required: false}]}
+      >
+      <Avatar/> 
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Demo;

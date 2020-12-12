a499e7f44c6164b50c3b45bbec0fc4c136200e63
haild173094 <56255783+haild173094@users.noreply.github.com>
2020-12-12 20:06:35 +0700

    Revert "Revert "Merge branch 'taiha' of https://github.com/Nhac14/frontend-nhac14 into taiha""
    This reverts commit b1175b77dbbc756b17a62d01b365817b6ec6d6cd.

--- src/pages/Admin/Album/form-create-album.jsx
+++ src/pages/Admin/Album/form-create-album.jsx
@@ -1,6 +1,12 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Space, Table, Button, Modal, Form, Input, Checkbox, Select } from 'antd';
+import { useEffect, useState } from 'react';
 import SelectWithHiddenSelectedOptions from '../../Helper/Selected-input-component.jsx';
 import Avatar from '../../Helper/Upload-image-preview.jsx';
+import categoryApi from '../../../api/category';
+import songApi from '../../../api/song';
+import singerApi from '../../../api/singer';
+
+
 const layout = {
   labelCol: { span: 6 },
   wrapperCol: { span: 18 },
@@ -19,6 +25,113 @@ const Demo = () => {
   const onFinishFailed = errorInfo => {
     console.log('Failed:', errorInfo);
   };
+
+  const isEmpty = (obj) => {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key))
+        return false;
+    }
+    return true;
+  }
+
+  const [listCategory, setListCategory] = useState({});
+  const [listSinger, setListSinger] = useState({});
+  const [listSong, setListSong] = useState({});
+
+  const OPTIONS1 = !isEmpty(listCategory) ? listCategory.data.data.map(item => item.name) : [];
+
+  const [selectedCategory, setSelectedCategory] = useState([]);
+
+  const category = () => {
+    const handleChange = () => {
+      setSelectedCategory(categoryValue);
+    }
+    const categoryValue = selectedCategory;
+    const filteredOptions = OPTIONS1.filter(o => !categoryValue.includes(o));
+    return (
+      <Select
+        mode="tags"
+        placeholder="Inserted are removed"
+        //value={categoryValue}
+        // value={}
+        onChange={handleChange}
+        style={{ width: '100%' }}
+      >
+        {filteredOptions.map(item => (
+          <Select.Option key={item} value={item}>
+            {item}
+          </Select.Option>
+        ))}
+      </Select>
+    );
+  }
+
+  const [selectedSingers, setSelectedSingers] = useState([]);
+
+  const OPTIONS2 = !isEmpty(listSinger) ? listSinger.data.data.map(item => item.name) : [];
+
+
+  const singers = () => {
+    const handleChange = () => {
+      setSelectedSingers(singersValue);
+    }
+    const singersValue = selectedSingers;
+    const filteredOptions = OPTIONS2.filter(o => !singersValue.includes(o));
+    return (
+      <Select
+        mode="tags"
+        placeholder="Inserted are removed"
+        value={singersValue}
+        onChange={handleChange}
+        style={{ width: '100%' }}
+      >
+        {filteredOptions.map(item => (
+          <Select.Option key={item} value={item}>
+            {item}
+          </Select.Option>
+        ))}
+      </Select>
+    );
+  }
+
+  const [selectedSongs, setSelectedSongs] = useState([]);
+
+  const OPTIONS3 = !isEmpty(listSong) ? listSong.data.results.map(item => item.name) : [];
+
+  const songs = () => {
+    const handleChange = () => {
+      setSelectedSongs(songsValue);
+    }
+    const songsValue = selectedSongs;
+    const filteredOptions = OPTIONS3.filter(o => !songsValue.includes(o));
+    return (
+      <Select
+        mode="tags"
+        placeholder="Inserted are removed"
+        value={songsValue}
+        onChange={handleChange}
+        style={{ width: '100%' }}
+      >
+        {filteredOptions.map(item => (
+          <Select.Option key={item} value={item}>
+            {item}
+          </Select.Option>
+        ))}
+      </Select>
+    );
+  }
+
+  const handleAsync = async () => {
+    let tmp1 = await categoryApi.getAllCategory();
+    let tmp2 = await songApi.getAllSongAsync();
+    let tmp3 = await singerApi.getAllSingerAsync();
+
+    setListCategory(tmp1);
+    setListSinger(tmp3);
+    setListSong(tmp2);
+  }
+
+  useEffect(() => handleAsync(), []);
 
   return (
     <Form
@@ -39,33 +152,42 @@ const Demo = () => {
       <Form.Item
         label="Category"
         name="category"
-        rules={[{ required: true, message: 'you must be choose a category!' }]}
+        rules={[{ required: true, message: 'Please input category\'s name!' }]}
       >
-        <SelectWithHiddenSelectedOptions/>
+        {category()}
       </Form.Item>
 
       <Form.Item
-        label="Single"
-        name="single"
-        rules={[{ required: true, message: 'you must be choose a single' }]}
+        label="Singers"
+        name="singers"
+        rules={[{ required: true, message: 'Please input singer\'s name!' }]}
       >
-        <SelectWithHiddenSelectedOptions/>
+        {singers()}
       </Form.Item>
+
+      <Form.Item
+        label="Songs"
+        name="songs"
+        rules={[{ required: true, message: 'Please input song\'s name!' }]}
+      >
+        {songs()}
+      </Form.Item>
+
 
       <Form.Item
         label="Description"
         name="description"
-        rules={[{ required: false}]}
+        rules={[{ required: false }]}
       >
-        <TextArea rows={6}/>
+        <TextArea rows={6} />
       </Form.Item>
 
       <Form.Item
-        label="Description"
-        name="description"
-        rules={[{ required: false}]}
+        label="Avatar"
+        name="avatar"
+        rules={[{ required: false }]}
       >
-      <Avatar/> 
+        <Avatar />
       </Form.Item>
 
       <Form.Item {...tailLayout}>
--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -5,7 +5,6 @@ import Home from '../Home/Home';
 
 export default function MainLayout({children, user, logout}){
 
-    console.log("Mainlayout: ", user);
     return (
         <div>
             <HeaderX user={user} logout={(e) => logout(e)}/>
